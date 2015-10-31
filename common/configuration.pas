1af19e22b4075c83a81638bc9524aec5ba3c1532
Nikita Zuev <nikitazu@gmail.com>
2015-10-31 20:17:59 +0300

    refactoring

--- common/configuration.pas
+++ common/configuration.pas
@@ -17,40 +17,40 @@ type
     function GetSolutionPath(projectName: String): String;
     function GetOrCreateConfigFile: TINIFile;
   private
-    appDir: String;
-    databasesDir: String;
-    configPath: String;
+    _appDir: String;
+    _databasesDir: String;
+    _configPath: String;
   end;
 
 implementation
 
 constructor TConfig.Create;
 begin
-  appDir := GetAppConfigDirUTF8(false, true);
-  databasesDir := AppendPathDelim(appDir) + AppendPathDelim('db');
-  configPath := appDir + 'config.ini';
+  _appDir := GetAppConfigDirUTF8(false, true);
+  _databasesDir := AppendPathDelim(_appDir) + AppendPathDelim('db');
+  _configPath := _appDir + 'config.ini';
 end;
 
 function TConfig.GetConfigPath: String;
 begin
-  Result := configPath;
+  Result := _configPath;
 end;
 
 function TConfig.GetDatabasesPath: String;
 begin
-  Result := databasesDir;
+  Result := _databasesDir;
 end;
 
 function TConfig.GetDatabasePath(projectName: String): String;
 begin
-  Result := databasesDir + projectName;
+  Result := _databasesDir + projectName;
 end;
 
 function TConfig.GetSolutionPath(projectName: String): String;
 var
   config: TIniFile;
 begin
-  config := TIniFile.Create(configPath);
+  config := TIniFile.Create(_configPath);
   try
     Result := config.ReadString('links', projectName, '');
   finally
@@ -60,12 +60,12 @@ end;
 
 function TConfig.GetOrCreateConfigFile: TINIFile;
 begin
-  if not FileExistsUTF8(configPath) then
+  if not FileExistsUTF8(_configPath) then
   begin
-    Result := TIniFile.Create(configPath);
+    Result := TIniFile.Create(_configPath);
     Result.WriteString('system', 'foo', 'bar');
   end
-  else Result := TIniFile.Create(configPath);
+  else Result := TIniFile.Create(_configPath);
 end;
 
 end.
