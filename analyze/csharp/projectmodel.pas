14e90a87a08bdd9ccf083c67c9f8ae620ed19be4
Nikita Zuev <nikitazu@gmail.com>
2015-11-01 01:31:46 +0300

    Load project model title.

--- analyze/csharp/projectmodel.pas
+++ analyze/csharp/projectmodel.pas
@@ -5,23 +5,28 @@ unit ProjectModel;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils,
+  FileUtil;
 
 type
   TProjectModel = Class(TComponent)
     private
      _path: String;
+     _title: String;
     public
       procedure Load(path: String);
     published
       property Path: String read _path write _path;
+      property Title: String read _title write _title;
   end;
 
 implementation
 
 procedure TProjectModel.Load(path: String);
 begin
-  WriteLn('TODO ', path);
+  if FileExistsUTF8(path) then begin
+    Title := ExtractFileNameOnly(path);
+  end;
 end;
 
 end.
---
+++ analyze/csharp/projectmodeltests.pas
@@ -0,0 +1,45 @@
+unit ProjectModelTests;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, fpcunit, testutils, testregistry,
+  ProjectModel;
+
+type
+
+  TProjectModelTests= class(TTestCase)
+  private
+    _project: TProjectModel;
+  protected
+    procedure SetUp; override;
+    procedure TearDown; override;
+  published
+    procedure Load;
+  end;
+
+implementation
+
+procedure TProjectModelTests.Load;
+begin
+  _project.Load('..\testdata\analyze\csharp\SimpleWallet.Core\SimpleWallet.Core.csproj');
+  AssertEquals('Title', 'SimpleWallet.Core', _project.Title);
+end;
+
+procedure TProjectModelTests.SetUp;
+begin
+  _project := TProjectModel.Create(nil);
+end;
+
+procedure TProjectModelTests.TearDown;
+begin
+  FreeAndNil(_project);
+end;
+
+initialization
+
+  RegisterTest(TProjectModelTests);
+end.
+
