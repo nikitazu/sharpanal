33d5b5c181c89637a6e6710fb3624eeec0b3bf2a
Nikita Zuev <nikitazu@gmail.com>
2015-11-01 02:06:54 +0300

    Project model load files

--- analyze/csharp/projectmodel.pas
+++ analyze/csharp/projectmodel.pas
@@ -6,26 +6,63 @@ interface
 
 uses
   Classes, SysUtils,
-  FileUtil;
+  FileUtil,
+  Laz2_DOM, laz2_XMLRead;
 
 type
   TProjectModel = Class(TComponent)
     private
-     _path: String;
-     _title: String;
+      _path: String;
+      _title: String;
+      _files: TStringList;
     public
+      constructor Create(aOwner: TComponent); override;
+      destructor Destroy; override;
       procedure Load(path: String);
     published
       property Path: String read _path write _path;
       property Title: String read _title write _title;
+      property Files: TStringList read _files write _files;
   end;
 
 implementation
 
+constructor TProjectModel.Create(aOwner: TComponent);
+begin
+  inherited;
+  _files := TStringList.Create;
+end;
+
+destructor TProjectModel.Destroy;
+begin
+  FreeAndNil(_files);
+  inherited;
+end;
+
 procedure TProjectModel.Load(path: String);
+var
+  document: TXMLDocument;
+  itemGroup: TDOMNode;
+  item: TDOMNode;
+  itemFile: String;
 begin
   if FileExistsUTF8(path) then begin
     Title := ExtractFileNameOnly(path);
+    try
+      ReadXMLFile(document, path);
+      itemGroup := document.DocumentElement.FindNode('ItemGroup');
+      while Assigned(itemGroup) do begin
+        item := itemGroup.FindNode('Compile');
+        while Assigned(item) do begin
+          itemFile := item.Attributes.GetNamedItem('Include').TextContent;
+          _files.Add(itemFile);
+          item := item.NextSibling;
+        end;
+        itemGroup := itemGroup.NextSibling;
+      end;
+    finally
+      FreeAndNil(document);
+    end;
   end;
 end;
 
--- analyze/csharp/projectmodeltests.pas
+++ analyze/csharp/projectmodeltests.pas
@@ -26,6 +26,83 @@ procedure TProjectModelTests.Load;
 begin
   _project.Load('..\testdata\analyze\csharp\SimpleWallet.Core\SimpleWallet.Core.csproj');
   AssertEquals('Title', 'SimpleWallet.Core', _project.Title);
+  AssertEquals('Files count', 19, _project.Files.Count);
+
+  AssertEquals('File 1',
+    'ComponentModel\PropertyChangedNotifyer.cs',
+    _project.Files[0]);
+
+  AssertEquals('File 2',
+    'DataProcessing\DateGroup.cs',
+    _project.Files[1]);
+
+  AssertEquals('File 3',
+    'DataProcessing\DateGroupType.cs',
+    _project.Files[2]);
+
+  AssertEquals('File 4',
+    'DataProcessing\GroupByTime.cs',
+    _project.Files[3]);
+
+  AssertEquals('File 5',
+    'DataProcessing\GroupByMonth.cs',
+    _project.Files[4]);
+
+  AssertEquals('File 6',
+    'Exporters\ExportableTransaction.cs',
+    _project.Files[5]);
+
+  AssertEquals('File 7',
+    'Exporters\QifExporter.cs',
+    _project.Files[6]);
+
+  AssertEquals('File 8',
+    'Exporters\QifType.cs',
+    _project.Files[7]);
+
+  AssertEquals('File 9',
+    'Extensions\DateTimeOffsetExt.cs',
+    _project.Files[8]);
+
+  AssertEquals('File 10',
+    'Extensions\JsonObjectExt.cs',
+    _project.Files[9]);
+
+  AssertEquals('File 11',
+    'Extensions\TimeSpanExt.cs',
+    _project.Files[10]);
+
+  AssertEquals('File 12',
+    'Extensions\StringParseExt.cs',
+    _project.Files[11]);
+
+  AssertEquals('File 13',
+    'Properties\AssemblyInfo.cs',
+    _project.Files[12]);
+
+  AssertEquals('File 14',
+    'UI\Converters\BoolToVisibility.cs',
+    _project.Files[13]);
+
+  AssertEquals('File 15',
+    'UI\Converters\CurrencyToString.cs',
+    _project.Files[14]);
+
+  AssertEquals('File 16',
+    'UI\Converters\DateTimeToDateTimeOffset.cs',
+    _project.Files[15]);
+
+  AssertEquals('File 17',
+    'UI\Converters\DateToString.cs',
+    _project.Files[16]);
+
+  AssertEquals('File 18',
+    'UI\Converters\DecimalToString.cs',
+    _project.Files[17]);
+
+  AssertEquals('File 19',
+    'UI\Converters\TimeToString.cs',
+    _project.Files[18]);
 end;
 
 procedure TProjectModelTests.SetUp;
