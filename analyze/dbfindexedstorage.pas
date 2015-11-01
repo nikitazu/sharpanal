c850572127634119612433e6e7098ef7addeb52c
Nikita Zuev <nikitazu@gmail.com>
2015-11-01 05:02:41 +0300

    Append data

--- analyze/dbfindexedstorage.pas
+++ analyze/dbfindexedstorage.pas
@@ -24,6 +24,20 @@ type
     public
       constructor Create(aOwner: TComponent); override;
       procedure CreateTables(path: String);
+
+      function AppendSolution(title: String): Integer;
+
+      function AppendProject(
+        title: String;
+        path: String;
+        solutionId: Integer): Integer;
+
+      function AppendFile(
+        title: String;
+        path: String;
+        solutionId: Integer;
+        projectId: Integer): Integer;
+
       property IsDebug : Boolean write _isDebug;
   end;
 
@@ -40,15 +54,49 @@ end;
 
 procedure TDbfIndexedStorage.CreateTables(path: String);
 begin
-  Log('TDbfIndexedStorage create tables');
+  Log('create tables');
   ForceDirectories(path);
-  Log('TDbfIndexedStorage create table solutions');
+  Log('create table solutions');
   CreateSolutionsTable(path);
-  Log('TDbfIndexedStorage create table projects');
+  Log('create table projects');
   CreateProjectsTable(path);
-  Log('TDbfIndexedStorage create table files');
+  Log('create table files');
   CreateFilesTable(path);
-  Log('TDbfIndexedStorage create tables done');
+  Log('create tables done');
+end;
+
+function TDbfIndexedStorage.AppendSolution(title: String): Integer;
+begin
+  Log('append solution ' + title);
+  with _solutionsTable do begin
+    AppendRecord([nil, title]);
+    Result := FieldValues['solution_id'];
+  end;
+end;
+
+function TDbfIndexedStorage.AppendProject(
+  title: String;
+  path: String;
+  solutionId: Integer): Integer;
+begin
+  Log('append project ' + title);
+  with _projectsTable do begin
+    AppendRecord([nil, title, path, solutionId]);
+    Result := FieldValues['project_id'];
+  end;
+end;
+
+function TDbfIndexedStorage.AppendFile(
+  title: String;
+  path: String;
+  solutionId: Integer;
+  projectId: Integer): Integer;
+begin
+  Log('append file ' + title);
+  with _projectsTable do begin
+    AppendRecord([nil, title, path, solutionId, projectId]);
+    Result := FieldValues['file_id'];
+  end;
 end;
 
 procedure TDbfIndexedStorage.CreateSolutionsTable(path: String);
@@ -59,19 +107,19 @@ begin
       TableLevel := 7;
       Exclusive := True;
       TableName := 'solutions.dbf';
-      Log('TDbfIndexedStorage solutions add columns');
+      Log('solutions add columns');
       with FieldDefs do begin
         Add('solution_id', ftAutoInc, 0, True);
         Add('title', ftString, 100, True);
       end;
-      Log('TDbfIndexedStorage solutions write to disk');
+      Log('solutions write to disk');
       CreateTable;
       Open;
-      Log('TDbfIndexedStorage solutions index');
+      Log('solutions index');
       AddIndex('ix_s_sid', 'solution_id', [ixPrimary, ixUnique]);
       AddIndex('ix_s_title', 'title', [ixCaseInsensitive, ixUnique]);
     finally
-      Log('TDbfIndexedStorage close table solutions');
+      Log('close table solutions');
       Close;
     end;
   end;
@@ -85,21 +133,23 @@ begin
       TableLevel := 7;
       Exclusive := True;
       TableName := 'projects.dbf';
-      Log('TDbfIndexedStorage projects add columns');
+      Log('projects add columns');
       with FieldDefs do begin
         Add('project_id', ftAutoInc, 0, True);
         Add('title', ftString, 100, True);
+        Add('path', ftString, 255, True);
         Add('solution_id', ftInteger, 0, True);
       end;
-      Log('TDbfIndexedStorage projects write to disk');
+      Log('projects write to disk');
       CreateTable;
       Open;
-      Log('TDbfIndexedStorage projects index');
+      Log('projects index');
       AddIndex('ix_p_pid', 'project_id', [ixPrimary, ixUnique]);
       AddIndex('ix_p_title', 'title', [ixCaseInsensitive, ixUnique]);
+      AddIndex('ix_p_path', 'path', [ixCaseInsensitive, ixUnique]);
       AddIndex('ix_ps_sid', 'solution_id', []);
     finally
-      Log('TDbfIndexedStorage close table projects');
+      Log('close table projects');
       Close;
     end;
   end;
@@ -113,23 +163,25 @@ begin
       TableLevel := 7;
       Exclusive := True;
       TableName := 'files.dbf';
-      Log('TDbfIndexedStorage files add columns');
+      Log('files add columns');
       with FieldDefs do begin
         Add('file_id', ftAutoInc, 0, True);
         Add('title', ftString, 100, True);
+        Add('path', ftString, 255, True);
         Add('solution_id', ftInteger, 0, True);
         Add('project_id', ftInteger, 0, True);
       end;
-      Log('TDbfIndexedStorage files write to disk');
+      Log('files write to disk');
       CreateTable;
       Open;
-      Log('TDbfIndexedStorage files index');
+      Log('files index');
       AddIndex('ix_f_f_id', 'file_id', [ixPrimary, ixUnique]);
-      AddIndex('ix_f_title', 'title', [ixCaseInsensitive, ixUnique]);
+      AddIndex('ix_f_title', 'title', [ixCaseInsensitive]);
+      AddIndex('ix_f_path', 'path', [ixCaseInsensitive]);
       AddIndex('ix_fs_sid', 'solution_id', []);
       AddIndex('ix_fp_pid', 'project_id', []);
     finally
-      Log('TDbfIndexedStorage close table files');
+      Log('close table files');
       Close;
     end;
   end;
